30e1bcc1441d975eeeafce7c270f9755cf57c6fc
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-06-25 01:05:05 +0300

    Created findAll method and getForTable method in HotelManager and HotelDao

--- src/business/HotelManager.java
+++ src/business/HotelManager.java
@@ -1,2 +1,46 @@
-package business;public class HotelManager {
+package business;
+
+import dao.HotelDao;
+import entity.Hotel;
+import entity.User;
+
+import java.util.ArrayList;
+
+public class HotelManager {
+
+    private final HotelDao hotelDao;
+
+    public HotelManager() {
+        this.hotelDao = new HotelDao();
+    }
+
+    public ArrayList<Hotel> findAll() {
+        return this.hotelDao.findAll();
+    }
+
+    public ArrayList<Object[]> getForTable(int size, ArrayList<Hotel> hotels) {
+        ArrayList<Object[]> hotelList = new ArrayList<>();
+        for (Hotel hotel:hotels) {
+            int i = 0;
+            Object[] rowObject = new Object[size];
+            rowObject[i++] = hotel.getId();
+            rowObject[i++] = hotel.getName();
+            rowObject[i++] = hotel.getCity();
+            rowObject[i++] = hotel.getRegion();
+            rowObject[i++] = hotel.getAddress();
+            rowObject[i++] = hotel.getEmail();
+            rowObject[i++] = hotel.getPhone();
+            rowObject[i++] = hotel.getStar();
+            rowObject[i++] = hotel.isFreeParking();
+            rowObject[i++] = hotel.isFreeWifi();
+            rowObject[i++] = hotel.isSwimmingPool();
+            rowObject[i++] = hotel.isGym();
+            rowObject[i++] = hotel.isHotelConcierge();
+            rowObject[i++] = hotel.isSpa();
+            rowObject[i] = hotel.isRoomService();
+            hotelList.add(rowObject);
+        }
+        return hotelList;
+    }
+
 }
--- src/dao/HotelDao.java
+++ src/dao/HotelDao.java
@@ -1,2 +1,51 @@
-package dao;public class HotelDao {
+package dao;
+
+import core.Database;
+import entity.Hotel;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class HotelDao {
+
+    private final Connection connection;
+
+    public HotelDao() {
+        this.connection = Database.getInstance();
+    }
+
+    public ArrayList<Hotel> findAll() {
+        ArrayList<Hotel> hotelList = new ArrayList<>();
+        String query = "SELECT * FROM public.hotel";
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Hotel hotel = new Hotel();
+                hotel.setId(resultSet.getInt("hotel_id"));
+                hotel.setName(resultSet.getString("hotel_name"));
+                hotel.setCity(resultSet.getString("city"));
+                hotel.setRegion(resultSet.getString("region"));
+                hotel.setAddress(resultSet.getString("address"));
+                hotel.setEmail(resultSet.getString("email"));
+                hotel.setPhone(resultSet.getString("phone"));
+                hotel.setStar(Hotel.Star.valueOf(resultSet.getString("star")));
+                hotel.setFreeParking(resultSet.getBoolean("freeparking"));
+                hotel.setFreeWifi(resultSet.getBoolean("freewifi"));
+                hotel.setSwimmingPool(resultSet.getBoolean("swimmingpool"));
+                hotel.setGym(resultSet.getBoolean("gym"));
+                hotel.setHotelConcierge(resultSet.getBoolean("concierge"));
+                hotel.setSpa(resultSet.getBoolean("spa"));
+                hotel.setRoomService(resultSet.getBoolean("roomservice"));
+                hotelList.add(hotel);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return hotelList;
+    }
+
 }
