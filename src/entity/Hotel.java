f638d67fbbdc51b21fead164ef97af5819f83979
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-06-24 23:11:45 +0300

    Created Hotel class in entity

---
+++ src/entity/Hotel.java
@@ -0,0 +1,24 @@
+package entity;
+
+public class Otel {
+
+    private String name;
+    private String address;
+    private String email;
+    private String phone;
+
+
+
+    public enum Star {
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE
+    }
+
+    public enum HotelType {
+
+    }
+
+}
