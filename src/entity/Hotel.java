8900a76e8b1b13fe1fb7bc828f059edd593a37aa
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-06-24 23:13:01 +0300

    Done Hotel Entity

--- src/entity/Hotel.java
+++ src/entity/Hotel.java
@@ -1,13 +1,23 @@
 package entity;
 
-public class Otel {
+public class Hotel {
 
+    private int id;
     private String name;
+    private String city;
+    private String region;
     private String address;
     private String email;
     private String phone;
-
-
+    private Star star;
+    private HotelType hotelType;
+    private boolean freeParking;
+    private boolean freeWifi;
+    private boolean swimmingPool;
+    private boolean gym;
+    private boolean hotelConcierge;
+    private boolean spa;
+    private boolean roomService;
 
     public enum Star {
         ONE,
@@ -18,7 +28,145 @@ public class Otel {
     }
 
     public enum HotelType {
+        ULTRA_ALL_INCLUSIVE,
+        ALL_INCLUSIVE,
+        BED_AND_BREAKFAST,
+        FULL_BOARD,
+        HALF_BOARD,
+        JUST_THE_BED,
+        FULL_CREDIT_EXCEPT_ALCOHOL
+    }
 
+    public Hotel() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Star getStar() {
+        return star;
+    }
+
+    public void setStar(Star star) {
+        this.star = star;
+    }
+
+    public HotelType getHotelType() {
+        return hotelType;
+    }
+
+    public void setHotelType(HotelType hotelType) {
+        this.hotelType = hotelType;
+    }
+
+    public boolean isFreeParking() {
+        return freeParking;
+    }
+
+    public void setFreeParking(boolean freeParking) {
+        this.freeParking = freeParking;
+    }
+
+    public boolean isFreeWifi() {
+        return freeWifi;
+    }
+
+    public void setFreeWifi(boolean freeWifi) {
+        this.freeWifi = freeWifi;
+    }
+
+    public boolean isSwimmingPool() {
+        return swimmingPool;
+    }
+
+    public void setSwimmingPool(boolean swimmingPool) {
+        this.swimmingPool = swimmingPool;
+    }
+
+    public boolean isGym() {
+        return gym;
+    }
+
+    public void setGym(boolean gym) {
+        this.gym = gym;
+    }
+
+    public boolean isHotelConcierge() {
+        return hotelConcierge;
+    }
+
+    public void setHotelConcierge(boolean hotelConcierge) {
+        this.hotelConcierge = hotelConcierge;
+    }
+
+    public boolean isSpa() {
+        return spa;
+    }
+
+    public void setSpa(boolean spa) {
+        this.spa = spa;
+    }
+
+    public boolean isRoomService() {
+        return roomService;
+    }
+
+    public void setRoomService(boolean roomService) {
+        this.roomService = roomService;
     }
 
 }
